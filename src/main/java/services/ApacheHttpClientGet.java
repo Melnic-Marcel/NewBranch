3171d00699ad096fe4aeaa1f2241cafc2439f16f
Melnic-Marcel <m6667.m@mail.ru>
2022-03-19 14:05:22 +0200

    Initial commit

--- src/main/java/services/ApacheHttpClientGet.java
+++ src/main/java/services/ApacheHttpClientGet.java
@@ -46,3 +46,4 @@ public class ApacheHttpClientGet {
         }
     }
 }
+//Edit File2
--- src/main/java/services/BNM/New_class.java
+++ src/main/java/services/BNM/New_class.java
@@ -3,7 +3,7 @@ package services.BNM;
 public class New_class {
         public static void main(String[] args)
         {
-            System.out.println("new file");
+            System.out.println("Edit File1**");
 
         }
     }
